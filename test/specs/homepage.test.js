b21063d911937f1d312f0a3f758bdcb270aca9ec
ofindleyqw <ofindley@qualityworkscg.com>
2021-09-20 15:17:34 -0500

    updated test to work with webdriverio version

--- test/specs/homepage.test.js
+++ test/specs/homepage.test.js
@@ -4,10 +4,10 @@ const homepageData = require('../data/homepage.data');
 //#endregion
 
 describe('Homepage', () => {
-    it('Verfy that the header title is displayed correctly', () => {
-        HomePage.open();
-        HomePage.headerTitle.waitForDisplayed();
-        expect(HomePage.headerTitle).toHaveText(homepageData.title);
+    it('Verfy that the header title is displayed correctly', async () => {
+        await HomePage.open();
+        await HomePage.headerTitle.waitForDisplayed();
+        await expect(HomePage.headerTitle).toHaveText(homepageData.title);
     });
 });
 
