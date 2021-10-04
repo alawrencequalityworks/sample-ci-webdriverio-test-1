c196280654afe2f6aba2a85789fd472513dc612d
ofindleyqw <ofindley@qualityworkscg.com>
2021-10-04 13:18:11 -0500

    added logic to skip intro page

--- test/specs/homepage.test.js
+++ test/specs/homepage.test.js
@@ -4,7 +4,7 @@ const homepageData = require('../data/homepage.data');
 //#endregion
 
 describe('Homepage', () => {
-    it('Verfy that the header title is displayed correctly', async () => {
+    beforeEach('Skip intro page', async function () {
         await HomePage.open();
 
         console.log('Url: ' + await browser.getUrl());
@@ -12,9 +12,13 @@ describe('Homepage', () => {
         console.log('Continue Button: ' + await HomePage.continueBtn.isDisplayed());
         console.log('Error: ' + await HomePage.error.isDisplayed());
 
-        // await HomePage.h2.waitForDisplayed();
-        // await HomePage.headerTitle.waitForDisplayed();
-        //await expect(HomePage.headerTitle).toHaveText(homepageData.title);
+        if (await HomePage.continueBtn.isDisplayed()) {
+            await HomePage.continueBtn.click();
+        }
+    });
+    it('Verfy that the header title is displayed correctly', async () => {
+        await HomePage.headerTitle.waitForDisplayed();
+        await expect(HomePage.headerTitle).toHaveText(homepageData.title);
     });
 });
 
