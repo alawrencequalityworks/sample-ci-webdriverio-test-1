dd6310f5e84153a396a833b6d5dadff6c7a87f09
ofindleyqw <ofindley@qualityworkscg.com>
2021-10-27 18:34:33 -0500

    clean up tests

--- test/specs/homepage.test.js
+++ test/specs/homepage.test.js
@@ -6,22 +6,15 @@ const homepageData = require('../data/homepage.data');
 describe('Homepage', () => {
     beforeEach('Skip intro page', async function () {
         await HomePage.open();
-
         console.log('Url: ' + await browser.getUrl());
-        console.log('Header Title: ' + await HomePage.headerTitle.isDisplayed());
-        console.log('Continue Button: ' + await HomePage.continueBtn.isDisplayed());
-        console.log('Error: ' + await HomePage.error.isDisplayed());
 
         if (await HomePage.continueBtn.isDisplayed()) {
             await browser.saveScreenshot(`./screenshots/beforeClick.png`);
             await HomePage.continueBtn.waitForClickable();
             await HomePage.continueBtn.click();
-            console.log('Was it here?');
         }
     });
     it('Verfy that the header title is displayed correctly', async () => {
-        console.log('NEW Continue Button??: ' + await HomePage.continueBtn.isDisplayed());
-        await browser.pause(5000);
         await HomePage.headerTitle.waitForDisplayed();
         await browser.saveScreenshot(`./screenshots/beforeHeader.png`);
         await expect(HomePage.headerTitle).toHaveText(homepageData.title);
