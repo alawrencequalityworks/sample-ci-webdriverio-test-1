31b17a94c1b933b45ce1550231e3f653c315d0dd
ofindleyqw <ofindley@qualityworkscg.com>
2021-10-27 22:40:58 -0500

    clean up tests

--- test/specs/homepage.test.js
+++ test/specs/homepage.test.js
@@ -4,19 +4,11 @@ const homepageData = require('../data/homepage.data');
 //#endregion
 
 describe('Homepage', () => {
-    beforeEach('Skip intro page', async function () {
+    it('Verfy that the header title is displayed correctly', async () => {
         await HomePage.open();
         console.log('Url: ' + await browser.getUrl());
 
-        if (await HomePage.continueBtn.isDisplayed()) {
-            await browser.saveScreenshot(`./screenshots/beforeClick.png`);
-            await HomePage.continueBtn.waitForClickable();
-            await HomePage.continueBtn.click();
-        }
-    });
-    it('Verfy that the header title is displayed correctly', async () => {
         await HomePage.headerTitle.waitForDisplayed();
-        await browser.saveScreenshot(`./screenshots/beforeHeader.png`);
         await expect(HomePage.headerTitle).toHaveText(homepageData.title);
     });
 });
