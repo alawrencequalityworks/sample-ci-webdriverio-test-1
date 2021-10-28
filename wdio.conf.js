b26677c5c746ae0c7cd0ed9d1184891d53c64c00
ofindleyqw <ofindley@qualityworkscg.com>
2021-10-27 20:10:18 -0500

    update wait for timeout

--- wdio.conf.js
+++ wdio.conf.js
@@ -36,7 +36,7 @@ exports.config = {
     logLevel: 'error',
     bail: 0,
     baseUrl: URL,
-    waitforTimeout: 10000,
+    waitforTimeout: 15000,
     connectionRetryTimeout: 120000,
     connectionRetryCount: 3,
     framework: 'mocha',
